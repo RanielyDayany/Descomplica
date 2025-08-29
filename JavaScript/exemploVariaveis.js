d3f319dd713a2a87d053fb13d0543c078a4fa022
Raniely Dayany <ranielydayany58@gmail.com>
2025-08-28 21:07:33 -0300

    Update exemploVariaveis.js

--- JavaScript/exemploVariaveis.js
+++ JavaScript/exemploVariaveis.js
@@ -15,8 +15,10 @@ exemploVar();
 
 //Exemplo de erro e correção
 var mensagem1 = "Olá,Faculdade Descomplica!Escopo externo!";
-//A var dentro do if so funciona quando eu chamo dentro daquela função,se eu só colocasse o console.log aqui sem essa nova var aqui em cima iria dar um erro
-console.log(mensagem1);
+/*
+    A var dentro do if so funciona quando eu chamo dentro daquela função,se eu só colocasse o console.log aqui sem essa nova var aqui em cima iria dar um erro
+*/
+    console.log(mensagem1);
 
 //Declaração de uma variável usando let
 function exemploLet(){
@@ -32,7 +34,9 @@ exemploLet()
 //Exemplo de erro e correção
 let mensagem = "Olá Descomplica!Let externo"
 console.log(mensagem);
-//A princípio dá um erro pois a variável var tem o mesmo nome "mensagem",por isso irei trocar o nome do var por mensagem1 lá em cima
+/*
+    A princípio dá um erro pois a variável var tem o mesmo nome "mensagem",por isso irei trocar o nome do var por mensagem1 lá em cima
+*/
 
 //Declaração de uma constante usando const
 function exemploConstante(){
@@ -47,13 +51,9 @@ exemploConstante();
 console.log(externo);
 
 //Exemplo externo tentar alterar constante global
-externo = "Nova mensagem";//Dá erro porque não pode alterar const
+externo = "Nova mensagem";
+//Dá erro porque não pode alterar const
 
-//Demonstrando o escopo global de var
-
-//Demonstrando o escopo de bloco de let
-
-//Exemplo do escopo inacessível
 
 
 
