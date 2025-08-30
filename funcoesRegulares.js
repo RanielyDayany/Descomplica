d76da7f5c8b92c8fa1dc7d334353e36ba5824c64
Raniely Dayany <ranielydayany58@gmail.com>
2025-08-30 17:52:30 -0300

    update aulas JavaScript

---
+++ funcoesRegulares.js
@@ -0,0 +1,31 @@
+//Definindo uma função regular com dois parâmetros
+function soma(a, b){
+    return a + b;
+}
+
+//Chamando a função soma e armazenando o resultado em uma variável
+let resultado = soma(13,29);
+
+//Exibindo o resultado da função soma
+console.log("O resultado da soma é:" +resultado);
+
+//Definindo uma função regular usando a palavra-chave function
+function saudacao(nome){
+    console.log(`Olá, ${nome}!`);
+}
+
+//Chamando a função saudação e passando um argumento
+saudacao("Raniely");
+
+//Definindo uma função regular sem parâmetros e sem retorno
+function mensagem(){
+    console.log("Está é uma mensagem de boas vindas!")
+}
+
+
+//Chamando a funcão mensagem
+mensagem()
+
+
+
+
