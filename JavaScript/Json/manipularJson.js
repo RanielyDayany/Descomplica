8ccf74eeea67688e8df8e79dc898fdd7daa9da50
Raniely Dayany <ranielydayany58@gmail.com>
2025-08-31 19:06:54 -0300

    update JavaScript

---
+++ JavaScript/Json/manipularJson.js
@@ -0,0 +1,11 @@
+//Exemplo:manipulação de dados Json
+
+//Objeto JSON inicial
+let pessoa = {
+    "nome": "João",
+    "idade": "30",
+    "endereco": {
+        "rua": "Rua Principal",
+        "numero"
+    }
+}
